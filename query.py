9cabedcc80ff260972ab8af2385a35c71843fd5a
Sahil Karn <skarn@emich.edu>
2024-03-18 22:44:15 +0000

    Adding a feature that allows to see all the movie list Updated query.py so that it has a new option which displays all the movie names #2

--- query.py
+++ query.py
@@ -5,15 +5,23 @@ def main():
     
     while True:
         print("\nMENU:")
+        print("1. List all movie names")
         print("q. Quit")
         
         choice = input("Enter your choice: ").lower()
         
-        if choice == 'q':
+        if choice == '1':
+            list_all_movies(movies)
+        elif choice == 'q':
             print("Exiting the program.")
             break
         else:
             print("Invalid choice. Please try again.")
+
+def list_all_movies(movies):
+    print("\nAll Movie Names:")
+    for movie in movies._movies:
+        print(movie['name'])
 
 if __name__ == "__main__":
     main()
